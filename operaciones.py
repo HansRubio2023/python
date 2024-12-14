0ad973f563b11327facb3fffec91c9be79ef428f
HansRubio2023 <126277906+HansRubio2023@users.noreply.github.com>
2024-12-14 09:21:20 -0300

    commit
    commit

---
+++ operaciones.py
@@ -0,0 +1,21 @@
+div=17/3
+print(div)
+
+divisor = 17 %3
+print(divisor)
+
+calculadora = (50 - 5 * 6) / 4
+print(calculadora)
+
+potencia = 5 ** 2
+print(potencia)
+
+potencia2 = 2 ** 7
+print(potencia2)
+
+s = 'First line.\nSecond line.'
+print(s)
+
+#Listas
+squares = [1, 4, 9, 16, 25]
+print(squares[-3])
